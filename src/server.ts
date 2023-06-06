a1ec3db1133030d89c65df99ff3e2c3aad122eaa
Redi Ahmad <redi.ngoding@gmail.com>
2023-06-06 16:47:05 +0700

    feat: :sparkles: initialize Hapi server

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { Server } from 'hapi';
+import { NotesService } from './services/inMemory/NotesService';
+import notesPlugin from './api/notes';
+
+const initServer = async () => {
+	const notesService = new NotesService();
+	const server = new Server({
+		port: 3000,
+		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
+	});
+
+	await server.register({
+		plugin: notesPlugin,
+		options: { service: notesService },
+	});
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+initServer();
