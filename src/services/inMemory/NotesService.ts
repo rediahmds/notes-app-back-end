1dbfd6ac20ab03d6117f53be5ac0f08a96a1f4d8
Redi Ahmad <redi.ngoding@gmail.com>
2023-06-06 14:48:16 +0700

    refactor: :recycle: changes in params signature and error message.
    Other changes:
    
    - Fix `Note` interface optional property logic problems.
    
    - Fix typo of `tag` in `Note` property. Change to `tags`

--- src/services/inMemory/NotesService.ts
+++ src/services/inMemory/NotesService.ts
@@ -1,16 +1,15 @@
-import { Request, ResponseToolkit } from 'hapi';
 import { nanoid } from 'nanoid';
 
 // TODO: Fix the approach of implementing optional properties
 // Cuz: At some point those optional props are EXTREMELY necessary, it's the main structure.
 // Suggestion: Create a class or another interface to make Optional<Note> only when adding new note
 interface Note {
-	id?: string;
+	id: string;
 	title: string;
 	body: string;
-	tag: string[];
-	createdAt?: Date;
-	updatedAt?: Date;
+	tags: string[];
+	createdAt: Date;
+	updatedAt: Date;
 }
 
 class NotesService {
@@ -21,11 +20,15 @@ class NotesService {
 
 	/**
 	 *
-	 * @param request request object
+	 * @param request request object. Just pass it. It'll be handled correctly
 	 * @returns note id if success. Otherwise, return error message
 	 */
-	public addNote(request: Request) {
-		const { title, body, tag } = request.payload as Note;
+	public addNote(requestPayload: {
+		title: string;
+		body: string;
+		tags: string[];
+	}) {
+		const { title = 'untitled', body, tags } = requestPayload;
 		const id = nanoid(8);
 
 		const createdAt = new Date();
@@ -35,7 +38,7 @@ class NotesService {
 			id,
 			title,
 			body,
-			tag,
+			tags,
 			createdAt,
 			updatedAt,
 		};
@@ -44,7 +47,7 @@ class NotesService {
 
 		const isNoteAdded = this.notes.filter(note => note.id === id).length > 0;
 
-		if (!isNoteAdded) throw new Error('Failed to add the note!');
+		if (!isNoteAdded) throw new Error('Catatan gagal ditambahkan');
 
 		return id;
 	}
@@ -57,19 +60,22 @@ class NotesService {
 		const note = this.notes.filter(n => n.id === id)[0];
 
 		if (!note) {
-			throw new Error(`Note with ID of ${id} could not be found!`);
+			throw new Error(`Catatan tidak ditemukan`);
 		}
 
 		return note;
 	}
 
-	public editNoteByID(id: string, request: Request) {
-		const { title, body, tag } = request.payload as Note;
+	public editNoteByID(
+		id: string,
+		requestPayload: { title: string; body: string; tags: string[] }
+	) {
+		const { title, body, tags } = requestPayload;
 
 		const noteIndex = this.notes.findIndex(note => note.id === id);
 
 		if (noteIndex === -1)
-			throw new Error('Edit note failed. Note ID could not be found');
+			throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
 
 		const updatedAt = new Date();
 
@@ -77,7 +83,7 @@ class NotesService {
 			...this.notes[noteIndex],
 			title,
 			body,
-			tag,
+			tags,
 			updatedAt,
 		};
 	}
@@ -86,10 +92,10 @@ class NotesService {
 		const noteIndex = this.notes.findIndex(note => note.id === id);
 
 		if (noteIndex === -1)
-			throw new Error('Failed to delete note. Note ID could not be found');
+			throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
 
 		this.notes.splice(noteIndex, 1);
 	}
 }
 
-export default NotesService;
+export { Note, NotesService };
